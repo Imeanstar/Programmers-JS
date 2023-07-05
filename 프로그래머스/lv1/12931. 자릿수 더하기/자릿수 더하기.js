10d7d43d9b0ff2fc2f580188c375a9f67829efbf
Imeanstar <81348938+Imeanstar@users.noreply.github.com>
2023-07-05 16:54:48 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.15 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.js
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.js
@@ -1,20 +1,22 @@
 function solution(n)
 {
-    let k = n;
+    //정답 제출용 변수
     var answer = 0;
-    let formul = 0 ;
+    //숫자 길이 구하기
     const length = String(n).length;
+    //길이만큼 나눠주기 위한 변수
     let hun = 10 ** length / 10;
     
     while(hun != 0){
-        answer += k / hun;
+        //입력받은 수 / hun (ex. 123 / 100 = 1.23)
+        answer += n / hun;
+        //answer을 정수화 시킴(1.23 -> 1)
         answer = parseInt(answer);
-        k = k % hun;
-        // k = parseInt(k);
+        //n을 hun으로 나눠줌 (ex. 123 -> 23)
+        n = n % hun;
+        //hun을 한 자리 줄여줌(ex. 100 -> 10)
         hun = hun / 10;
+        //반복
     }
-    
-    // console.log(hun);
-    
     return answer;
 }
