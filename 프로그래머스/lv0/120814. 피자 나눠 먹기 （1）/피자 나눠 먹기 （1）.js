dd16b1b8909f0e3f03bae97db59ab2489629d523
Imeanstar <81348938+Imeanstar@users.noreply.github.com>
2023-07-11 00:31:49 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.js
+++ 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.js
@@ -1,6 +1,10 @@
+//3점
 function solution(n) {
     var answer = 0;
     
+    //7로 나눠서 몫을 answer에 대입
+    //나머지가 있으면(true면) 1을더해주고
+    //나머지가 없으면(0 = false면) 아무것도 안해줌
     answer = Math.floor(n/7);
     n%7 ? answer++ : answer+=0 ;
     
