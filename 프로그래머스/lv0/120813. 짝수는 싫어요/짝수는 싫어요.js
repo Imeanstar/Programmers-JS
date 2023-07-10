a845a26394d5217e155f625badef27c2f9f68a84
Imeanstar <81348938+Imeanstar@users.noreply.github.com>
2023-07-10 11:14:55 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.js
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.js
@@ -1,5 +1,8 @@
 function solution(n) {
     var answer = [];
+    //삼항연산자를 써서 해보려했는데, continue가 적용이 안됨
+    //i%2 ? answer.push(i) : continue;
+    
     for(let i = 1 ;  i <= n ; i++){
         if(i%2 == 0){
             continue;
