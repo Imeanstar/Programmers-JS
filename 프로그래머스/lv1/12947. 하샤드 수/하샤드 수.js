1745156f1975178a14f7b432f1d1045660534760
Imeanstar <81348938+Imeanstar@users.noreply.github.com>
2023-07-11 23:30:12 +0900

    [level 1] Title: 하샤드 수, Time: 0.05 ms, Memory: 32.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.js
@@ -0,0 +1,14 @@
+function solution(x) {
+    
+    let i = String(x);
+    let arr = [...i];
+    let sum = 0;
+    var answer = true;
+    for(let j = 0 ; j < arr.length ; j++){
+        sum += parseInt(arr[j]);
+    }
+    Number.isInteger(x/sum) ? answer =  true : answer = false;
+    
+    
+    return answer;
+}
