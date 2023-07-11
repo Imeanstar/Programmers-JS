23a832813a6346835463f88d20b5c70aff4b9247
Imeanstar <81348938+Imeanstar@users.noreply.github.com>
2023-07-11 23:25:38 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.js
+++ 프로그래머스/lv1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.js
@@ -1,11 +1,16 @@
+//3점
 function solution(n) {
     let i = String(n);
     let arr = [];
     var answer = 0;
     arr = [...i];
+    
+    //sort method사용 (오름차순 정리)
     arr.sort(function(a,b){
         return a-b;
     });
+    
+    //내림차순으로 숫자가 되도록 더해 지수 사용해 더해준다
     for(let j = 0 ; j < arr.length ; j++){
         answer += arr[j] * (10**j);
     }
