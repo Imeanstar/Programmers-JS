7ac09e84057b4d1edb4a9d1d2a4a09126c5fca9c
Imeanstar <81348938+Imeanstar@users.noreply.github.com>
2023-07-18 01:12:38 +0900

    [level 0] Title: 직각삼각형 출력하기, Time: 45.61 ms, Memory: 32.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120823. 직각삼각형 출력하기/직각삼각형 출력하기.js
+++ 프로그래머스/lv0/120823. 직각삼각형 출력하기/직각삼각형 출력하기.js
@@ -1,3 +1,6 @@
+//1점
+//console.log는 출력하면 줄바꿈이 필연적으로 발생
+//process.stdout.write사용해서 줄바꿈없는 console.log 출력
 const readline = require('readline');
 const rl = readline.createInterface({
     input: process.stdin,
@@ -9,7 +12,6 @@ let input = [];
 rl.on('line', function (line) {
     input = line.split(' ');
 }).on('close', function () {
-    // console.log((input[0]));
     for(let i = 1 ; i <= input[0] ; i++){
         for(let j = 0 ; j < i ; j++){
             process.stdout.write('*');
