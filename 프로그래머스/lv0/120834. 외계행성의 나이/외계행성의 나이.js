98a714ace951caa02f23e1cd657d8cd50cc0f00c
Imeanstar <81348938+Imeanstar@users.noreply.github.com>
2023-08-09 07:19:18 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.js
+++ 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.js
@@ -1,3 +1,6 @@
+//1점
+//다른 사람들 보니까 map으로 많이 푼듯
+//문자열도 index로 접근 가능하다는 점 생각하기
 function solution(age) {
     var answer = [];
     let ten = 10;
@@ -5,7 +8,6 @@ function solution(age) {
         answer.push(String.fromCharCode(age%10+97));
         age = Math.floor(age/10);
     }
-    // console.log(ten);
     answer.reverse();
     answer = answer.join('')
     return answer;
