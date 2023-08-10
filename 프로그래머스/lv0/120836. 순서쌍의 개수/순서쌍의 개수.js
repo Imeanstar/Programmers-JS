542ffe742c3932b0eb89682465f73828467b1e78
Imeanstar <81348938+Imeanstar@users.noreply.github.com>
2023-08-10 23:54:31 +0900

    [level 0] Title: 순서쌍의 개수, Time: 0.20 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.js
+++ 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.js
@@ -1,20 +1,18 @@
+//2점
+//나는 그냥 해당 숫자의 배열칸에 넣었는데 push를 한 사람이 있네
+//와 그냥 약수의 갯수 구하면 되는거니까 가면서 push돌려서 몇개인지만 구하면 되네
 function solution(n) {
     var answer = 1;
     let arr= []
     let i = 2;
     while (n > 1){
-        
         if(Number.isInteger(n/i)){
-            if(isNaN(arr[i]) == true){
-                arr[i] = 0;
-            }
+            if(isNaN(arr[i]) == true)arr[i] = 0;
             arr[i]++;
             n = n/i;
             i = 2;
         }
-        else{
-            i++;
-        }
+        else i++;
     }
     
     for(let j = 0 ; j < arr.length ; j++){
