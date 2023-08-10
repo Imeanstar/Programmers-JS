196b3e7759ecc31f06af94aea61e4942a15aaae9
Imeanstar <81348938+Imeanstar@users.noreply.github.com>
2023-08-10 23:55:17 +0900

    [level 0] Title: 순서쌍의 개수, Time: 0.14 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.js
+++ 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.js
@@ -7,17 +7,28 @@ function solution(n) {
     let i = 2;
     while (n > 1){
         if(Number.isInteger(n/i)){
-            if(isNaN(arr[i]) == true)arr[i] = 0;
+            //만약 n/i가 정수라면
+            if(isNaN(arr[i]) == true){
+                //만약 arr[i]가 NaN or null 이라면
+                arr[i] = 0;
+                //1 더해줘야 하니까 0 으로 만들어준다
+            }
             arr[i]++;
             n = n/i;
             i = 2;
+            //1 더해주고, n을 i로 나눠서 다음 수로 바꾸고 i를 다시 2로 초기화
         }
-        else i++;
+        else{
+            i++;
+            //아니면 현재의 i는 약수가 아니므로 i에 1 더해주기
+        }
     }
     
     for(let j = 0 ; j < arr.length ; j++){
+        //arr배열 완성된 상태
         if(arr[j]){
             answer *= arr[j]+1
+            //약수의 갯수는 지수+1 의 곱
         }
     }
     
