8793893af8b7a564ae6163040e44e2e806be49f5
Imeanstar <81348938+Imeanstar@users.noreply.github.com>
2023-08-15 00:07:04 +0900

    [level 1] Title: [1차] 다트 게임, Time: 3.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.js
+++ 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.js
@@ -1,23 +1,21 @@
+//5점
+//아니 이거 왜 5점임?
+//코드 구성이 좀 어렵긴한데 꼬인부분은 없어서 그런가;
+//if문 말고 switch문으로 리팩토링해도.. 괜찮을지도 모르겠네
 function solution(dartResult) {
     var answer = 0;
     let arrNum = -1;//1or2or3 번째 다트
     let arr = [];
     for(let i = 0 ; i < dartResult.length ; i++){
-        //let temp = dartResult[i];
-        //temp = parseInt(temp);
-        //console.log('배열 = ' + dartResult[i]);
         if(isNaN(dartResult[i]) == false){
             if(dartResult[i] == 0 && dartResult[i-1]==1){
+                //만약 10 이면 1&0으로 인식해서 10으로 인식하도록
                 arr[arrNum] = 10;
             }
             else{
                 arrNum++;
                 arr.push(dartResult[i]);
             }
-            
-            //console.log(arr);
-            //console.log('배열 번호 = ' + arrNum);
-            //console.log(dartResult[arrNum]);
         }
         if(isNaN(dartResult[i]) == true){
             let tmp = dartResult[i];
@@ -43,8 +41,6 @@ function solution(dartResult) {
                 arr[arrNum] = arr[arrNum]*-1;
             }
         }
-        //arr[arrNum].push(dartResult[i]);
-        //arr[arrNum].push(dartResult[i]);
     }
     
     for(let j = 0 ; j < arr.length ; j++){
