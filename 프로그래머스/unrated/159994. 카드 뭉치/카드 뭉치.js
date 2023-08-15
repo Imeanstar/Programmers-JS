fb011b82363012f91e6e2815e1b1894e8b476b96
Imeanstar <81348938+Imeanstar@users.noreply.github.com>
2023-08-15 14:15:54 +0900

    [unrated] Title: 카드 뭉치, Time: 3.54 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.js
@@ -0,0 +1,20 @@
+function solution(cards1, cards2, goal) {
+    var answer = 'Yes';
+    
+    for(let i = 0 ; i < goal.length ; i++){
+        if(goal[i]==cards1[0]){
+            cards1.shift();
+            console.log(cards1);
+        }
+        else if(goal[i] == cards2[0]){
+            cards2.shift();
+            console.log(cards2);
+        }
+        else{
+            answer = "No";
+            break;
+        }
+    }
+    
+    return answer;
+}
