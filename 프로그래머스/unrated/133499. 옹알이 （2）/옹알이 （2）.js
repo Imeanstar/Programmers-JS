adb43f15a7a3ec99467172d73008ea180415ea0e
Imeanstar <81348938+Imeanstar@users.noreply.github.com>
2023-08-27 23:47:52 +0900

    [unrated] Title: 옹알이 (2), Time: 0.36 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.js
+++ 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.js
@@ -1,12 +1,17 @@
+//14점
 function solution(babbling) {
-    //babbling = ["mawoowoo"]
     var answer = 0;
     let arr = ['aya' , 'ye' , 'woo' , 'ma'];
     //index에 없으면 -1이라서 원래 -1로 해놨는데 -2로 변경함
-    let temp2 = -2;
-    //console.log(babbling);
+    //원래 여기였는데 에러때문에 for문 안으로 배치함
+    //let temp2 = -2;
     for(let i = 0 ; i < babbling.length ; i++){
+        //이거 없어서 좀 오래 에러 해결하느라 걸렸음
+        //배열이 ['ayaye', 'ye'] 이렇게 되어있으면 답이 2가 나와야되는데
+        //0번에서 ye로 끝나니까 temp2에 ye를 넣어둠
+        //그래서 다음 ye가 중복되는걸로 처리되고있었음
         let temp2 = -2;
+        //length가 계속 바껴서 그냥 고정
         let temp = babbling[i].length;
         for(let j = 0 ; j < temp ; j++){
             if(babbling[i][0]=='a' || babbling[i][0]=='w'){
@@ -18,7 +23,6 @@ function solution(babbling) {
                 //만약 잘라낸 부분(3자리) 가 aya거나 woo 라면
                 else if(arr.indexOf((babbling[i].slice(0,3))) != -1){
                     temp2 = arr.indexOf((babbling[i].slice(0,3)));
-                    console.log(temp2);
                     babbling[i] = babbling[i].slice(3,);
                     if(babbling[i] == ''){
                         break;
@@ -26,16 +30,12 @@ function solution(babbling) {
                 }
             }
             else if(babbling[i][0]=='y' || babbling[i][0] == 'm'){
-                //console.log((babbling[i].slice(0,2)))
                 if(arr.indexOf((babbling[i].slice(0,2))) == temp2){
-                    console.log('con');
                     temp2 = -2;
                     break;
                 }
                 if(arr.indexOf((babbling[i].slice(0,2))) != -1){
                     temp2 = arr.indexOf((babbling[i].slice(0,2)));
-                    console.log(temp2);
-                    console.log(babbling[i].slice(0,2));
                     babbling[i] = babbling[i].slice(2,);
                     if(babbling[i] == ''){
                         break;
@@ -49,6 +49,5 @@ function solution(babbling) {
             answer++;
         }
     }
-    //console.log(answer);
     return answer;
 }
